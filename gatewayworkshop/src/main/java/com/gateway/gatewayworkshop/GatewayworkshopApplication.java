f31716d162998e737518d99988d469eac36473c8
samantamath <samantamath25@gmail.com>
2024-03-05 11:57:37 +0530

    e-commerce

---
+++ gatewayworkshop/src/main/java/com/gateway/gatewayworkshop/GatewayworkshopApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.gatewayworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayworkshopApplication.class, args);
+	}
+
+}
---
+++ gatewayworkshop/src/test/java/com/gateway/gatewayworkshop/GatewayworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.gatewayworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
